8c6ad97708fe69c502a79fea894306074c3792d9
Filip <filip.olsen@hotmail.com>
2019-12-08 12:05:18 +0100

    Day 2.

--- one/src/main.rs
+++ one/src/main.rs
@@ -6,14 +6,16 @@ fn main() {
     part_two(&data);
 }
 
+pub fn fuel(mass: i32) -> i32 {
+    return (mass / 3) - 2 as i32;
+}
+
 pub fn part_one(data: &str) {
     let mut req: i32 = 0;
 
     for s in data.split("\n") {
-        let mass : i32 = s.trim().parse().unwrap();
-        let calc: f32 = (mass / 3) as f32;
-        let res = calc.floor() as i32 - 2;
-        req += res;
+        let mass: i32 = s.trim().parse().unwrap();
+        req = req + fuel(mass);
     }
 
     println!("Part One: {:?}", req);
@@ -23,16 +25,16 @@ pub fn part_two(data: &str) {
     let mut req: i32 = 0;
 
     for s in data.split("\n") {
-        let mass : i32 = s.trim().parse().unwrap();
+        let mass: i32 = s.trim().parse().unwrap();
         let res = calc_fuel(mass, 0);
         req += res.1;
     }
+
     println!("Part Two: {:?}", req);
 }
 
 pub fn calc_fuel(mass: i32, mut req: i32) -> (i32, i32) {
-    let calc: f32 = (mass / 3) as f32;
-    let res = calc.floor() as i32 - 2;
+    let res = fuel(mass);
 
     if res <= 0 {
         return (res, req);
